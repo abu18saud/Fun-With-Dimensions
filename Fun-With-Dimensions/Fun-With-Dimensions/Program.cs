f4314764d25fdbf5379c3cf6433b56e74ba98db8
Abdullah Alsalem <abdullahalsalem18@gmail.com>
2021-05-04 17:49:10 +0300

    Done

---
+++ Fun-With-Dimensions/Fun-With-Dimensions/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace FunWithDimensions
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int length = Convert.ToInt32(Console.ReadLine());
+            int width = Convert.ToInt32(Console.ReadLine());
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            Cuboid cuboid = new Cuboid(length, width, height);
+
+            Console.WriteLine("Volume: " + cuboid.Volume());
+            Console.WriteLine("Perimeter: " + cuboid.Perimeter());
+        }
+
+
+    }
+    struct Cuboid
+    {
+        public int length;
+        public int width;
+        public int height;
+
+        //create a constructor
+        public Cuboid(int length, int width, int height)
+        {
+            this.length = length;
+            this.width = width;
+            this.height = height;
+        }
+
+        //complete this method
+        public int Volume()
+        {
+            return length * width * height;
+        }
+        //complete this method
+        public int Perimeter()
+        {
+            return 4 * (length + width + height);
+        }
+    }
+}
